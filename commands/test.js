9765fa22622f80c6f3dbf4c8d2bcf786ce48afc1
dejafloppa <deja@mail.virtualhackers.cf>
2022-11-06 14:34:23 +0200

    main commit

---
+++ commands/test.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('discord.js');
+const mysql = require('mysql');
+const { dbhost, db, username, password } = require('../config.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('getmoney')
+		// eslint-disable-next-line quotes
+		.setDescription("Get a user's money")
+		.addStringOption(option =>
+			option.setName('user')
+				.setDescription('The user')),
+	async execute(interaction) {
+		const dbe = mysql.createConnection({
+			host: dbhost,
+			user: username,
+			password: password,
+			database: db,
+		});
+		const user = interaction.options.get('user');
+		const query = `SELECT cash FROM characters WHERE name = '${user}'`;
+		dbe.query(query, async (err, result) => {
+			if (err) throw err;
+			console.log(result);
+			await interaction.reply(result);
+		});
+	},
+};
